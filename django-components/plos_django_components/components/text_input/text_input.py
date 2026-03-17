ae62b04a47e98aab192d0c52be2127917a1cc092
Rose Reatherford <rreatherford@plos.org>
2026-03-17 11:27:35 -0500

    Refactor input box to accept different input types, removing need for password fields.

--- django-components/plos_django_components/components/text_input/text_input.py
+++ django-components/plos_django_components/components/text_input/text_input.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from django_components import register
 
 from ..base.base_component import PLOSBaseComponent
@@ -9,10 +11,12 @@ class TextInput(PLOSBaseComponent):
 
     def get_context_data(self, label, name, value: str = "", placeholder: str = "", required: bool = False,
                          hint: str | None = None, errors: list[str] | None = None,
+                         input_type: Literal["text", "number", "password", "email", "url"] = "text",
                          field_id: str | None = None, disabled: bool = False, maxlength: int | None = None,
                          minlength: int | None = None, prefix: str | None = None, suffix: str | None = None,):
         return {
             "label": label,
+            "input_type": input_type,
             "name": name,
             "value": value,
             "disabled": disabled,
