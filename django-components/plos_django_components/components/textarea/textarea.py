6f7c2be9e39a276e6c0991a7d843547b09360b12
Rose Reatherford <rreatherford@plos.org>
2026-03-18 08:20:59 -0500

    Update long text area.

--- django-components/plos_django_components/components/textarea/textarea.py
+++ django-components/plos_django_components/components/textarea/textarea.py
@@ -1,30 +1,39 @@
+from typing import Literal
+
 from django_components import register
 
 from ..base.base_component import PLOSBaseComponent
 
 
-@register("textarea")
+@register("plos_textarea")
 class Textarea(PLOSBaseComponent):
     template_name = "textarea.html"
 
-    def get_context_data(
-        self,
-        id: str,
-        name: str,
-        label: str,
-        hint: str | None = None,
-        value: str = "",
-        rows: int = 5,
-        maxlength: int | None = None,
-        errors: list[str] | None = None,
-    ):
-        return {
-            "id": id,
-            "name": name,
-            "label": label,
-            "hint": hint,
-            "value": value,
-            "rows": rows,
-            "maxlength": maxlength,
-            "errors": errors or [],
-        }
+    def get_context_data(self,
+                         id: str,
+                         name: str,
+                         label: str | None = None,
+                         label_size: Literal["large", "medium", "small"] = "small",
+                         hint: str | None = None,
+                         placeholder: str | None = None,
+                         required: bool = False,
+                         value: str | None = None,
+                         rows: int = 5,
+                         cols: int | None = None,
+                         maxlength: int | None = None,
+                         minlength: int | None = None,
+                         errors: list[str] | None = None, ):
+        return {"id": id,
+                "name": name,
+                "label": label,
+                "label_size": label_size,
+                "hint": hint,
+                "placeholder": placeholder,
+                "required": required,
+                "value": value,
+                "rows": rows,
+                "cols": cols,
+                "maxlength": maxlength,
+                "minlength": minlength,
+                "errors": errors or [],
+                }
