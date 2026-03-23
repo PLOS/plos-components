5e237282f4002226f2d9b1e6922bddfe9c1a4351
Rose Reatherford <rreatherford@plos.org>
2026-03-23 10:48:52 -0500

    Update the JS.

--- django-components/plos_django_components/components/base/base_component.py
+++ django-components/plos_django_components/components/base/base_component.py
@@ -2,6 +2,7 @@
 A base component which provides the CSS and JS for all components.
 """
 
+from django.utils.safestring import mark_safe
 from django_components import Component
 
 
@@ -19,4 +20,24 @@ class PLOSBaseComponent(Component):
             "https://ux.plos.org/assets/v3/styles/gov-uk-frontend-v6.min.css",
             "https://ux.plos.org/assets/v3/styles/plos-overrides.min.css",
         ]
-        js = ["https://ux.plos.org/assets/v3/scripts/gov-uk-frontend-v6.min.js"]
+        js = [
+            mark_safe(
+                """
+                    <script>
+      document.body.className +=
+        " js-enabled" +
+        ("noModule" in HTMLScriptElement.prototype
+          ? " govuk-frontend-supported"
+          : "");
+    </script>
+                    """
+            ),
+            mark_safe(
+                """
+<script type="module">
+      import { initAll } from "https://ux.plos.org/assets/v3/scripts/gov-uk-frontend-v6.min.js";
+      initAll();
+    </script>
+                    """
+            ),
+        ]
