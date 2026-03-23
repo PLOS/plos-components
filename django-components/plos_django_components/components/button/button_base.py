51cad7d1020029a66b3a9850dc92fc193e752f10
Rose Reatherford <rreatherford@plos.org>
2026-03-23 08:13:32 -0500

    Update  button to add value field for form submission.

--- django-components/plos_django_components/components/button/button_base.py
+++ django-components/plos_django_components/components/button/button_base.py
@@ -1,3 +1,10 @@
+"""
+A component function which provides the ability to create a button on any web page.
+
+This module provides:
+- A button that can be displayed on any web page.
+"""
+
 from typing import Literal
 
 from django_components import register
@@ -6,10 +13,14 @@ from ..base.base_component import PLOSBaseComponent
 
 
 @register("plos_button")
-class TextBox(PLOSBaseComponent):
+class Button(PLOSBaseComponent):
+    """
+    A button that can be displayed on any web page.
+    """
+
     template_name = "button_base.html"
 
-    def get_context_data(
+    def get_context_data(  # noqa: D102
         self,
         /,
         *,
@@ -18,6 +29,7 @@ class TextBox(PLOSBaseComponent):
         button_type: Literal["button", "reset", "submit"] = "button",
         icon: str | None = None,
         icon_position: Literal["right", "left"] = "right",
+        value: str | None = None,
     ):
         return {
             "disabled": disabled,
@@ -25,4 +37,5 @@ class TextBox(PLOSBaseComponent):
             "button_type": button_type,
             "icon": icon,
             "icon_position": icon_position,
+            "value": value,
         }
