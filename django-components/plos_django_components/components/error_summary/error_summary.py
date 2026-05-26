39112ff666f93601a096c46b2e105d01908cb4bb
Nevelina Aleksandrova <naleksandrova@plos.org>
2026-05-26 16:10:40 +0100

    Fix component import

--- django-components/plos_django_components/components/error_summary/error_summary.py
+++ django-components/plos_django_components/components/error_summary/error_summary.py
@@ -6,7 +6,7 @@ anchor links to the affected fields.
 """
 from django_components import register
 
-from ..base.base_component import PLOSBaseComponent
+from ..components.base.base_component import PLOSBaseComponent
 
 
 @register("plos_error_summary")
